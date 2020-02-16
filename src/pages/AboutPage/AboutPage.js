8a74765755fee182b43e9d076d84004a2af2f1e1
davidgeorgebell <bell.dave93@gmail.com>
2020-02-16 13:29:40 +0800

    about information

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -4,8 +4,21 @@ import './AboutPage.css';
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className='about-page-wrapper'>
+      <h1>About</h1>
+      <p className='about-info'>This is a site built by David Bell.</p>
+      <ul className='about-list'>
+        <li>
+          <a href='https://github.com/davidgeorgebell' alt='github account'>
+            Github
+          </a>
+        </li>
+        <li>
+          <a href='mailto:davidbell.dev@gmail.com' alt='email address'>
+            Email
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
