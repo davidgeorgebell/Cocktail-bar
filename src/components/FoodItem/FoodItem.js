5863ac36f4c8751f2ef4a946a352c55a31aabbca
davidgeorgebell <bell.dave93@gmail.com>
2020-02-18 08:49:39 +0800

    food menu styles

--- src/components/FoodItem/FoodItem.js
+++ src/components/FoodItem/FoodItem.js
@@ -5,13 +5,18 @@ import './FoodItem.css';
 const FoodItem = ({ meal }) => {
   return (
     <div className='food-item-wrapper'>
-      <img
-        className='food-item-image'
-        src={meal.strMealThumb}
-        alt={meal.strMeal}
-      />
-      <h3 className='food-item-title'>{meal.strMeal}</h3>
-      {/* <p className='food-item-desc'>{meal.strArea}</p> */}
+      <div className='picture'>
+        <img
+          className='food-item-image'
+          src={meal.strMealThumb}
+          alt={meal.strMeal}
+        />
+      </div>
+      <div className='description'>
+        <h3 className='food-item-title'>{meal.strMeal}</h3>
+        <h1 className='price'>£10.00</h1>
+        <p className='food-item-desc'>{meal.strArea}</p>
+      </div>
     </div>
   );
 };
